86b812708efc6c7563e20fffea8c130ed3816bfc
giout <85247301+giout@users.noreply.github.com>
2023-03-23 16:09:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-const grid = new Grid({title: "Grid", columns: ["key", "value"], rows: 4});
-const container = document.getElementById("container");
-container.appendChild(grid);
 
-grid.addRows([["one", "1"], ["two", "2"]]);
