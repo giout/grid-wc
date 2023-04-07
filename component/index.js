97dd37bd8fc055f0af932bcd97f283cfb75247af
giout <giovanniurdaneta10@gmail.com>
2023-04-07 01:11:28 -0400

    Optimizadas las propiedades para mostrar barra y modificar titulo y agregada la propiedad de mostrar titulo

--- component/index.js
+++ component/index.js
@@ -9,10 +9,7 @@ class Grid extends HTMLElement
         this._indexPage = 0; // Indice de la pagina.
         this._currentRow = 0; // Contador de fila actual.
 
-        if (this.properties.title == undefined) this.properties.title = "";
-
         // La grilla tiene por defecto la barra de controles sin columna de items.
-        if (this.properties.showBar == undefined) this.properties.showBar = true;
         if (this.properties.showItem == undefined) this.properties.showItem = false;
 
         // Se agrega la columna item.
@@ -35,45 +32,38 @@ class Grid extends HTMLElement
         // Titulo
         this._divTitle = document.createElement("div");
         this._divTitle.className = "_title _border";
-        this._divTitle.innerHTML = this.properties.title;
 
         // Barra de controles
-        if (this.properties.showBar)
-        { 
-            const bt = document.createElement("template");
-            bt.innerHTML = 
-            `<div class="_control">
-                <button class="_bar _button _border"><<</button>
-                <button class="_bar _button _border"><</button>
-                <div class="_bar _page _border"></div>
-                <button class="_bar _button _border">></button>
-                <button class="_bar _button _border">>></button>
-            </div>`;
+        const bt = document.createElement("template");
+        bt.innerHTML = 
+        `<div class="_control">
+            <button class="_bar _button _border"><<</button>
+            <button class="_bar _button _border"><</button>
+            <div class="_bar _page _border"></div>
+            <button class="_bar _button _border">></button>
+            <button class="_bar _button _border">>></button>
+        </div>`;
 
-            this._bar = bt.content.cloneNode(true).querySelector("._control");
-            const barElements = this._bar.getElementsByClassName("_bar"); // 4 botones y guia.
-            
-            // Guia de paginas
-            this._guide = barElements[2];
-            
-            // Botones
-            barElements[0].addEventListener("click", e => this.firstPage());
-            barElements[1].addEventListener("click", e => this.previousPage());
-            barElements[3].addEventListener("click", e => this.nextPage());
-            barElements[4].addEventListener("click", e => this.lastPage());
+        this._bar = bt.content.cloneNode(true).querySelector("._control");
+        const barElements = this._bar.getElementsByClassName("_bar"); // 4 botones y guia.
+        
+        // Guia de paginas
+        this._guide = barElements[2];
+        
+        // Botones
+        barElements[0].addEventListener("click", e => this.firstPage());
+        barElements[1].addEventListener("click", e => this.previousPage());
+        barElements[3].addEventListener("click", e => this.nextPage());
+        barElements[4].addEventListener("click", e => this.lastPage());
 
-            for (let i=0; i<barElements.length; i++)
-            {
-                barElements[i].style.fontSize = `${this.properties.barFontSize}px`;
-                barElements[i].style.color = this.properties.barFontColor;
-                barElements[i].style.backgroundColor = this.properties.barBackColor;
-                barElements[i].style.border = `solid ${this.properties.borderColor} ${this.properties.borderSize}px`;
-            }
-
-            // Se ajustan los porcentajes de filas del contenedor y se agrega.
-            this._container.style.gridTemplateRows = "20% 70% 10%";
-        } 
-
+        for (let i=0; i<barElements.length; i++)
+        {
+            barElements[i].style.fontSize = `${this.properties.barFontSize}px`;
+            barElements[i].style.color = this.properties.barFontColor;
+            barElements[i].style.backgroundColor = this.properties.barBackColor;
+            barElements[i].style.border = `solid ${this.properties.borderColor} ${this.properties.borderSize}px`;
+        }
+    
         // Agregando estilos.
         this._container.style.width = `${this.properties.size}px`;
         this._container.style.height = `${this.properties.size}px`;
@@ -82,17 +72,16 @@ class Grid extends HTMLElement
         this._divTitle.style.backgroundColor = `${this.properties.titleBackColor}`;
         this._divTitle.style.color = `${this.properties.titleFontColor}`;
 
-    }
-
-    connectedCallback()
-    {
-        // En caso de que aun no se hayan insertado datos, se agrega la primera pagina o grilla.
-        if (this._pages.length==0) this._pages.push(this._setGrid());  
+        // Se agrega la primera pagina o grilla.
+        this._pages.push(this._setGrid());  
 
         this.appendChild(this._container);
-        this._container.appendChild(this._divTitle);
         this._container.appendChild(this._pages[this._indexPage]);  
-        this._container.appendChild(this._bar);
+
+        // Propiedades por defecto.
+        this.title = "";
+        this.showTitle = true;
+        this.showBar = true;
     }
 
     // Recibe un array de objetos y lo agrega a la grilla.
@@ -132,15 +121,6 @@ class Grid extends HTMLElement
         this._indexPage = this._pages.length-1;
         this._switchPage(this._pages[this._indexPage]);
         this._updateGuide();
-    }
-
-    // Cargar al grid archivos de json locales.
-    loadJSON(path)
-    {
-        // Se solicita el archivo JSON.
-        fetch(path)
-        .then(response => response.json())
-        .then(data => this.addRows(data));
     }
 
     // Establece las caracteristicas de cada grilla.
@@ -215,9 +195,9 @@ class Grid extends HTMLElement
     _switchPage(div)
     {
         this._removeAllChilds(this._container);
-        this._container.appendChild(this._divTitle); // Se agrega titulo.
+        if (this._showTitle) this._container.appendChild(this._divTitle); 
         this._container.appendChild(div); // Se despliega la grilla.
-        if (this.properties.showBar) this._container.appendChild(this._bar);
+        if (this._showBar) this._container.appendChild(this._bar); 
     }
 
     // Metodo html desacoplado.
@@ -235,7 +215,7 @@ class Grid extends HTMLElement
     // La guia que muestra en que pagina se encuentra y el total de las mismas.
     _updateGuide()
     {
-        if (this.properties.showBar)
+        if (this._showBar)
         {
             this._guide.innerHTML = `${this._indexPage+1} / ${this._pages.length}`;
         }
@@ -265,6 +245,80 @@ class Grid extends HTMLElement
         // Bordes
         if (this.properties.borderColor == undefined) this.properties.borderColor = "black";
         if (this.properties.borderSize == undefined) this.properties.borderSize = 1;
+    }
+
+    set title(t)
+    {
+        this._divTitle.innerHTML = t;
+    }
+
+    set showTitle(t)
+    {
+        this._showTitle = t;
+        const firstNode = this._container.children[0];
+
+        // En ambos casos, se comprueba si la barra ha sido agregada. 
+        // Se comprueba si el titulo no ha sido agregado aun.
+        if (t)
+        {
+            if (firstNode.className != "_title _border")
+            {
+                if (this._showBar == false || this._showBar == undefined)
+                {
+                    this._container.style.gridTemplateRows = "20% 80%";
+                }
+                else
+                {
+                    this._container.style.gridTemplateRows = "20% 70% 10%";
+                }
+                this._switchPage(this._pages[0]);
+            }
+        }
+        else
+        {
+            // Caso contrario...
+            if (firstNode.className == "_title _border")
+            {
+                if (this._showBar == false || this._showBar == undefined)
+                {
+                    this._container.style.gridTemplateRows = "100%";
+                }
+                else
+                {
+                    this._container.style.gridTemplateRows = "80% 20%";
+                }
+                this._switchPage(this._pages[0]);
+            }
+        }
+    }
+
+    set showBar(sb)
+    {
+        this._showBar = sb;
+        const lastNode = this._container.children[this._container.children.length-1];
+
+        // En ambos casos se comprueba si el titulo ha sido agregado.
+        if (sb)
+        { 
+            // Se comprueba si la grilla ya tiene la barra agregada. Para esto, se extrae el ultimo hijo del div del container y se comprueba si no tiene el nombre de la clase correspondiente.
+            if (lastNode.className != "_control")
+            {
+                // Se ajustan los porcentajes de filas del contenedor y se agrega.
+                if (this._showTitle) this._container.style.gridTemplateRows = "20% 70% 10%";
+                else this._container.style.gridTemplateRows = "80% 20%";
+                this._switchPage(this._pages[0]);
+            }
+        } 
+        else
+        {
+            // Caso contrario...
+            if (lastNode.className == "_control")
+            {                
+                if (this._showTitle) this._container.style.gridTemplateRows = "20% 80%";
+                else this._container.style.gridTemplateRows = "100%";
+                this._switchPage(this._pages[0]);
+            } 
+        }
     }
 }
 
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let g = new Grid({columns: ["jugador", "equipo"], rows: 3});
+g.addRows([["Lionel Messi", "Fc Barcelona"], ["Ilkay Gundogan", "Manchester City"], ["Marco Reus", "Borrussia Dortmund"], ["Vinicius Jr.", "Real Madrid"], ["Karim Benzema", "Real Madrid"]])
 
+document.body.appendChild(g);
+g.showBar = false;
+g.showBar = true;
+g.showTitle = false;
+g.showBar =false;
+g.showTitle=true;
+g.showBar = true;
+g.showTitle = false;
+g.showBar = false;
